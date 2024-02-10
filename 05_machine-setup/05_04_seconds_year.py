dac422880c2ce186d67472f4af7c05b5092eb284
Gary <gary@gtolson.com>
2024-02-10 13:15:53 -0600

    python 05 lesson

--- 05_machine-setup/05_04_seconds_year.py
+++ 05_machine-setup/05_04_seconds_year.py
@@ -1,2 +1,9 @@
 # Use the interpreter to calculate how many seconds are in a year.
 # Then write the code in this script file below this line.
+
+secyear = 60*60*24*365
+secyear = str(secyear)
+
+print(f"There are {secyear} seconds in a year")
+print("There are "+ secyear +" seconds in a year")
+print("There are {} seconds in a year".format(secyear))
